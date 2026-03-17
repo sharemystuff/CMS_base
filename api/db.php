9cf3866e8b1229146167be7cc50ba8343c583965
sharemystuff <share@microstudio.cl>
2026-03-17 01:07:48 -0300

    config reemplaza a db

--- api/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-try {
-    $conexion = new mysqli('localhost', 'root', 'marcelo', 'cms_base');
-    $conexion->set_charset('utf8mb4');
-} catch (Exception $e) { header('Location: /tovi/pacheco.php'); exit; }
